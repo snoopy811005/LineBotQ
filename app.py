a6b7b57eaf9945bfd9b39c98df1ec9bfc6c719b0
snoopy811005 <91734861+snoopy811005@users.noreply.github.com>
2022-01-12 18:32:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -263,13 +263,13 @@ def getImageMessage(originalContentUrl):
 
 
 def replyMessage(payload):
-    response = requests.post("https://api.line.me/v2/bot/message/reply", headers=HEADER, data=payload)
+    response = requests.post("https://api.line.me/v2/bot/message/reply", headers=HEADER, data=json.dumps(payload))
     print(response.text)
     return 'OK'
 
 
 def pushMessage(payload):
-    response = requests.post("https://api.line.me/v2/bot/message/push", headers=HEADER, data=payload)
+    response = requests.post("https://api.line.me/v2/bot/message/push", headers=HEADER, data=json.dumps(payload))
     print(response.text)
     return 'OK'
 
