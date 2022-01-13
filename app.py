6c8c882bc3c5dd9b2f352c8bc1848ea510b84c36
snoopy811005 <91734861+snoopy811005@users.noreply.github.com>
2022-01-13 14:30:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,9 +48,9 @@ def index():
                 elif text == "出去玩囉":
                     payload["messages"] = [getPlayStickerMessage()]
                 elif text == "台北101":
-                    payload["messages"] = [getTaipei101ImageMessage()]
+                    payload["messages"] = [getTaipei101ImageMessage(),
 #                                            getTaipei101LocationMessage(),
-#                                            getMRTVideoMessage()]
+                                           getMRTVideoMessage()]
                 elif text == "扣打":
                     payload["messages"] = [
                             {
@@ -238,7 +238,9 @@ def getTaipei101LocationMessage():
 
 def getMRTVideoMessage():
     message = dict()
-
+    message["type"] = "video"
+    message["originalContentUrl"] = F"{end_point}/static/taipei_101_video.mp4"
+    message["previewImageUrl"] = F"{end_point}/static/taipei_1.jpeg"
     return message
 
 
