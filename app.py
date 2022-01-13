bb4105dc5dff62323ed33420832631b9b7be7188
snoopy811005 <91734861+snoopy811005@users.noreply.github.com>
2022-01-13 15:09:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -249,12 +249,12 @@ def getMRTSoundMessage():
     message = dict()
     message["type"] = "audio"
     message["originalContentUrl"] = F"{end_point}/static/mrt_sound.m4a"
-    message["duration"] = 33000
-#     import audioread
-#     with audioread.audio_open('static/mrt_sound.m4a') as f:
-#         # totalsec contains the length in float
-#         totalsec = f.duration
-#     message["duration"] = totalsec * 1000
+#     message["duration"] = 33000
+    import audioread
+    with audioread.audio_open('static/mrt_sound.m4a') as f:
+        # totalsec contains the length in float
+        totalsec = f.duration
+    message["duration"] = totalsec * 1000
     return message
 
 
